a589d9adce88cc5ea46198db1488d5864509f2a7
Laurent Hulstaert <laurenthulstaert@gmail.com>
2018-04-10 20:06:35 +0200

    run deleteRandomSaltdUser() when logout

--- logout.php
+++ logout.php
@@ -2,6 +2,8 @@
 
   require_once "srv/_config.php";
 
+  (new User($db))->deleteRandomSaltdUser($_SESSION['email']);
+
   unset($_SESSION['access_token']);
 
   $gClient->revokeToken();
@@ -11,5 +13,5 @@
   header('Location: index.php');
 
   exit();
-  
+
 ?>
