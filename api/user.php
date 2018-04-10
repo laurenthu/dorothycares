95a9aac83c1a56972732213d1677f6ce1cd9ee27
Laurent Hulstaert <laurenthulstaert@gmail.com>
2018-04-10 20:07:18 +0200

    add new route (user information)

--- api/user.php
+++ api/user.php
@@ -69,6 +69,17 @@
       header('Content-type: application/json');
       echo json_encode($json);
 
+    } elseif ($type == 'informationUser') {
+
+      $json['request']['status'] = 'success';
+      $json['request']['message'] = 'Congrats. You have all the requested information.';
+      $json['response'] = $u->getUserInformation($email);
+
+      // we return all the information in json
+      header('Access-Control-Allow-Origin: *');
+      header('Content-type: application/json');
+      echo json_encode($json);
+
     }
 
   }
