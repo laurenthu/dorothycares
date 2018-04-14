cdcce51d7dee6f83c832e915bc08087d5a7fc026
Cali93 <c.armut93@outlook.com>
2018-04-14 21:17:20 +0200

    cloud functions updated

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -7,8 +7,15 @@ const https = require('https');
 
 /* DOROTHY'S FUNCTIONS*/
 
+/* RESSOURCES */
 const RESSOURCES_ACTION = 'coding.technologies.ressources';
 const TECH_ARGUMENT = 'codingTechnology';
+
+const RELATIONAL_ACTION = 'user.becode.getCoach';
+const EMAIL_AGRGUMENT = 'email';
+const TOKEN_AGRGUMENT = 'token';
+
+const RELATIONAL_URL = 'https://dev.dorothycares.io/api';
 
 exports.dorothyCares = functions.https.onRequest((request, response) => {
   // 1. declaring our app so that we can make requests
@@ -26,8 +33,8 @@ exports.dorothyCares = functions.https.onRequest((request, response) => {
     console.log('give give ressources v1.5');
     let technology = app.getArgument(TECH_ARGUMENT);
     // make the request to our api to have the informations
-    console.log('https://dorothycares.herokuapp.com/ressources/'+technology);
-    https.get('https://dorothycares.herokuapp.com/ressources/'+technology, (res) => {
+    console.log('https://dorothycares.herokuapp.com/ressources/' + technology);
+    https.get('https://dorothycares.herokuapp.com/ressources/' + technology, (res) => {
       // declaring the body
       let body = '';
       // checking the status of the request
@@ -42,10 +49,61 @@ exports.dorothyCares = functions.https.onRequest((request, response) => {
         body = JSON.parse(body);
         console.log('on res body', body);
         body.modal = true;
+        body.api = 'no-rel';
         body = JSON.stringify(body);
         console.log('on res body stringify', body);
 
-        app.ask('Hello from give ressource'+ body);
+        app.ask('Hello from give ressource' + body);
+
+      });
+      // Handling erros
+    }).on('error', (e) => {
+      console.error(e);
+    });
+  }
+
+  function giveCoaches(app) {
+    // let siteName = app.getArgument(SITE_NAME_ARGUMENT);
+    // Keeping track of the version of the function that will be deployed
+    console.log('give give coaches v2.5');
+    let email;
+    let token;
+    // make the request to our api to have the informations
+    let sessionId = request.body.sessionId;
+    console.log('sessionId:', sessionId);
+    https.get(RELATIONAL_URL + '/session/' + sessionId + '/', (res) => {
+      // declaring the body
+      let body = '';
+      // checking the status of the request
+      console.log('statusCode:', res.statusCode);
+      // On response, fill the data inside the body
+      res.on('data', (data) => {
+        body += data;
+      });
+      // Once the body is filled with the informations
+      res.on('end', () => {
+        // parse the body
+        body = JSON.parse(body);
+        console.log('on res body', body);
+
+        email = body.response.email;
+        token = body.response.token;
+        console.log('response email', email);
+        console.log('response token', token);
+        https.get(RELATIONAL_URL + '/user/' + token + '/' + email + '/coach/', (res) => {
+          let body = '';
+          console.log('statusCode:', res.statusCode);
+          res.on('data', (data) => {
+            body += data;
+          });
+          res.on('end', () => {
+            body = JSON.parse(body);
+            body.api = 'rel';
+            console.log('on res body second', body);
+          })
+        })
+
+        app.ask('Hello from give coaches');
 
       });
       // Handling erros
@@ -59,6 +117,7 @@ exports.dorothyCares = functions.https.onRequest((request, response) => {
   // 4. build an action map, which maps intent names to functions
   let actionMap = new Map();
   actionMap.set(RESSOURCES_ACTION, giveRessources);
+  actionMap.set(RELATIONAL_ACTION, giveCoaches);
   // actionMap.set(END_ACTION, endConversation);
 
   app.handleRequest(actionMap);
