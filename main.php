0f70f184f8de58a3ff1b61094c473ddb2e22020c
Laurent Hulstaert <laurenthulstaert@gmail.com>
2018-04-19 08:20:38 +0200

    add Google Tag Manager

--- main.php
+++ main.php
@@ -40,9 +40,21 @@
     <meta name="twitter:site" content="@becodeorg">
     <meta name="twitter:image" content="https://dorothycares.io/img/printscreen.jpg">
 
+  <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-WCDFFXV');</script>
+
 </head>
 
 <body id="body" data-email="<?= $_SESSION['email']; ?>" data-token="<?= $_SESSION['token'] ?>" data-dialogflow-session="<?= session_id();?>">
+
+  <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WCDFFXV"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
 
   <!--Canvas container for the particles-->
 	<canvas id="canvas"></canvas>
